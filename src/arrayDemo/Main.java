46bca2ce814290d80efb0ef408099fd5a0245d05
ahmetozcan9211 <4ahmetozcan@gmail.com>
2022-09-17 17:13:53 +0300

    ders 13 son

---
+++ src/arrayDemo/Main.java
@@ -0,0 +1,25 @@
+package arrayDemo;
+
+public class Main {
+    public static void main (String [] args){
+        String ogrenci1="Engin";
+        String ogrenci2 = "Deniz";
+        String ogrenci3 = "Salih";
+        String ogrenci4 = "Ahmet";
+        System.out.println(ogrenci1);
+        System.out.println(ogrenci2);
+        System.out.println(ogrenci3);
+        System.out.println(ogrenci4);
+
+        System.out.println("------------------------");
+        String [] ogrenciler= new String[3];
+        ogrenciler[0]="ahmet";
+        ogrenciler[1]="deniz";
+        ogrenciler[2]="engin";
+
+        for(int i=0;i<ogrenciler.length;i++){
+            System.out.println(ogrenciler[i]);
+        }
+
+    }
+}
