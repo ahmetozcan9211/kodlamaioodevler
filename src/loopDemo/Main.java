c87b10a799ad7e8a5e8c901390a3512923516d28
ahmetozcan9211 <4ahmetozcan@gmail.com>
2022-09-17 17:05:41 +0300

    ders 11 son

--- src/loopDemo/Main.java
+++ src/loopDemo/Main.java
@@ -5,6 +5,21 @@ public class Main {
         for (int i=1;i <=9;i++){
             System.out.println(i);
         }
-        System.out.println("Döngü bitti");
+        System.out.println("For Döngü bitti");
+
+        int i = 1;
+        while(i<10){
+            System.out.println(i);
+            i+=2;
+        }
+        System.out.println("While döngüsü bitti");
+
+        int j =1;
+        do{
+            System.out.println(j);
+            j++;
+
+        }while(j<10);
+        System.out.println("do while dongusu bitti");
     }
 }
