13fb608d9aa877fef86c75c524361edc4c7f1cb0
ahmetozcan9211 <4ahmetozcan@gmail.com>
2022-09-18 10:34:55 +0300

    ders 17 son

---
+++ src/stringDemo/Main.java
@@ -0,0 +1,18 @@
+package stringDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        String mesaj = "bugun hava cok guzel";
+        System.out.println(mesaj);
+        System.out.println("Eleman sayısı: " + mesaj.length());
+        System.out.printf("5. elaman: " + mesaj.charAt(4));
+        System.out.println(mesaj.concat(" Yaşasın"));
+        System.out.println(mesaj.startsWith("a"));
+        System.out.println(mesaj.endsWith("l"));
+        char[] karakterler = new char[20];
+        mesaj.getChars(0, 20, karakterler,0);
+        System.out.println(karakterler);
+
+
+    }
+}
