fb146872451df5e4bcd1552ef94d6fd77e35fcb6
ahmetozcan9211 <4ahmetozcan@gmail.com>
2022-09-18 10:41:13 +0300

    ders 18 son

--- src/stringDemo/Main.java
+++ src/stringDemo/Main.java
@@ -3,7 +3,7 @@ package stringDemo;
 public class Main {
     public static void main(String[] args) {
         String mesaj = "bugun hava cok guzel";
-        System.out.println(mesaj);
+        /*System.out.println(mesaj);
         System.out.println("Eleman sayısı: " + mesaj.length());
         System.out.printf("5. elaman: " + mesaj.charAt(4));
         System.out.println(mesaj.concat(" Yaşasın"));
@@ -11,7 +11,18 @@ public class Main {
         System.out.println(mesaj.endsWith("l"));
         char[] karakterler = new char[20];
         mesaj.getChars(0, 20, karakterler,0);
-        System.out.println(karakterler);
+        System.out.println(karakterler);*/
+
+
+        System.out.println(mesaj.replace("a","-"));
+        System.out.println(mesaj.substring(2,4));
+
+        for (String kelime:mesaj.split(" ")){
+            System.out.println(kelime);
+
+        }
+        System.out.println(mesaj.toLowerCase());
+        System.out.println(mesaj.toUpperCase());
 
 
     }
