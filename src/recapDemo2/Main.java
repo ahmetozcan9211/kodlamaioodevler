07aa455a3ccd24389997f5eefbd73d3480db1f59
ahmetozcan9211 <4ahmetozcan@gmail.com>
2022-09-18 10:03:28 +0300

    ders 14 son

---
+++ src/recapDemo2/Main.java
@@ -0,0 +1,26 @@
+package recapDemo2;
+
+public class Main {
+    public static void main(String[] args) {
+        double[] myList = {1.2, 9.4, 6.7, 7.2};
+        double total = 0;
+        double biggest = myList[0];
+
+        for (double sayi : myList) {
+            total = total + sayi;
+
+            System.out.println(sayi);
+
+
+        }
+        System.out.println("Toplam = " + total);
+
+        for (double number : myList) {
+            if (biggest < number) {
+                biggest = number;
+            }
+        }
+        System.out.println("En büyük sayi = " + biggest);
+
+    }
+}
